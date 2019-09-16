211bcee0b38db9411f4f5a07d5d5679a1f4d8db3
Eddie O'Donnell <eddieod88@gmail.com>
2019-09-16 12:10:42 +0100

    determined the optimised values of theta using bfgs, created a vectorised prediction method, f_score wrapper and appropriate tests

---
+++ classification.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import fmin_bfgs
+from sklearn.metrics import f1_score
+
+
+class Classification:
+    """
+        Overall aim of this class is to determine the values for the vector, theta. Theta will be stored in a file
+        such that it can be used for real the testing and validation data.
+
+        Save the training data in a data frame and the tags themselves as a vector.
+
+        Functions required:
+            - Sigmoid function
+            - Cost function and gradient function
+            - Use fminunc using oct2py to determine theta.
+    """
+
+    def __init__(self, X: pd.DataFrame, y: pd.Series):
+        self.X = X  # Training dataframe
+        self.y = y  # supervised predictions of trainig data - must be codified.
+        self.theta = pd.Series  # Optimisation matrix
+        self.m = len(y)
+
+    def train(self, theta_init: pd.Series):
+        """
+            Determine the optimized value for theta such that predictions can be made on new data using the predict method.
+
+        :param theta_init: initial values for theta
+        :return: optimized theta vector which can be used in the predict method. Should perhaps store this vector in a file.
+        """
+        optimise = fmin_bfgs(self._cost, theta_init.values, fprime=self._gradient)
+        return optimise
+
+    def classify(self, theta_optim: pd.Series, test_X: pd.DataFrame = None):
+        """
+            Predict the classes of a new data set based on the optimised values of theta - if none, use the training data
+
+        :param theta_optim: optimised theta - vector/pd.series
+        :param test_X: test dataframe
+        :return: a vector of the predictions
+        """
+        if test_X:
+            X = test_X
+        else:
+            X = self.X
+        return self._predict(np.dot(theta_optim, X.T))
+
+    @staticmethod
+    def f_score(true_vals, predicted_vals):
+        """
+            Method used to get a basic f_score of the model. Shows how well the predictions align with the true classifications
+
+        :param true_vals: vector of y-values
+        :param predicted_vals: vector of predictions
+        :return: the f_score - a value between 0 and 1 - 1 being perfect and 0 being poor predictions (roughly speaking)
+        """
+        return f1_score(true_vals, predicted_vals)
+
+    @staticmethod
+    def _sigmoid(x):
+        """
+            This is the hypothesis function. It is used to eventually minimise theta
+
+        :param x: vector of theta^T * X (or scalar)
+        :return: Sigmoid of all of the values in the input vector
+        """
+        sig = 1 / (1 + np.exp(-x))
+        return sig
+
+    def _predict(self, x):
+        """
+        Vectorized implementation to go through the output of sig and anchor to 0 or 1 (for bin classification)
+
+        :param x:  vector of theta^T * X (or scalar)
+        :return:  prediction vector of 1's and 0's
+        """
+        sig = self._sigmoid(x)
+        sig = pd.Series(sig)
+        sig.where(sig.values >= 0.5, 0, inplace=True)  # if condition is FALSE, replace with value
+        sig.where(sig.values < 0.5, 1, inplace=True)
+        prediction = sig
+        return prediction
+
+    def _cost(self, theta: pd.Series):
+        #  For multi-class: https://ml-cheatsheet.readthedocs.io/en/latest/loss_functions.html
+        cost = -(1/self.m) * (np.dot(self.y, np.log(self._sigmoid(np.dot(theta, self.X.T)))) +
+                              np.dot((np.ones(len(self.y)) - self.y), np.log(np.ones(len(self.y)) -
+                                                                             self._sigmoid(np.dot(theta, self.X.T)))))
+        return cost
+
+    def _gradient(self, theta: pd.Series):
+        grad = (1/self.m) * np.dot(self._sigmoid(np.dot(theta, self.X.T)) - self.y, self.X)
+        return grad
--- tests/test_train.py
+++ tests/test_train.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 import pandas as pd
 
-from train import Train
+from classification import Classification
 
 
 class MyTestCase(unittest.TestCase):
@@ -28,17 +28,17 @@ class MyTestCase(unittest.TestCase):
         # Note: theta^T . X === X^T . theta
         dot = np.dot(self.theta, self.X.T)  # need to use np dot function as pandas only allows you to do the dot if columns have same name
         print(dot)
-        sig = Train._sigmoid(dot)
+        sig = Classification._sigmoid(dot)
         print(sig)
 
     def test_sigmoid_scalar(self):
         # for very negative numbers, sigmoid should equal zero, for very large, it should be 1
-        self.assertAlmostEqual(Train._sigmoid(-1000000), 0)
-        self.assertAlmostEqual(Train._sigmoid(1000000), 1)
+        self.assertAlmostEqual(Classification._sigmoid(-1000000), 0)
+        self.assertAlmostEqual(Classification._sigmoid(1000000), 1)
 
     def test_sigmoid_vector(self):
-        small_sig = Train._sigmoid(np.array([-10000, -10000, -10000]))
-        large_sig = Train._sigmoid(np.array([10000, 10000, 10000]))
+        small_sig = Classification._sigmoid(np.array([-10000, -10000, -10000]))
+        large_sig = Classification._sigmoid(np.array([10000, 10000, 10000]))
         for s_val, l_val in zip(small_sig, large_sig):
             self.assertAlmostEqual(s_val, 0)
             self.assertAlmostEqual(l_val, 1)
@@ -46,25 +46,32 @@ class MyTestCase(unittest.TestCase):
     def test_cost_zeros(self):
         # Cost should be 0.693 when theta is zeroed
         theta_z = pd.Series(np.zeros(len(self.X.columns)))
-        train = Train(self.X, self.y)
-        cost = train._cost(theta_z)
+        clsfy = Classification(self.X, self.y)
+        cost = clsfy._cost(theta_z)
         self.assertAlmostEqual(cost, 0.693, places=3)
 
     def test_grad(self):
         # Unsure what the grad should come to. but I think a vector of length = to number of features (+1 for intercept)
         theta_z = pd.Series(np.zeros(len(self.X.columns)))
-        train = Train(self.X, self.y)
-        grad = train._gradient(theta_z)
+        clsfy = Classification(self.X, self.y)
+        grad = clsfy._gradient(theta_z)
         print(grad)
-        # self.assertAlmostEqual(cost, 0.693, places=3)
 
     def test_optimise(self):
-        train = Train(self.X, self.y)
+        clsfy = Classification(self.X, self.y)
         theta_init = pd.Series(np.ones(len(self.X.columns)))
-        opt = train.optimise(theta_init)
+        opt = clsfy.train(theta_init)
         expected_vector = np.array([-25.161, 0.206, 0.201])
         for exp_el, result_el in zip(opt, expected_vector):
             self.assertAlmostEqual(result_el, exp_el, places=3)
+
+    def test_predict(self):
+        clsfy = Classification(self.X, self.y)
+        theta_init = pd.Series(np.ones(len(self.X.columns)))
+        opt = clsfy.train(theta_init)
+        prediction = clsfy._predict(np.dot(opt, self.X.T))
+        print(prediction)
+        print(clsfy.f_score(self.y, prediction))
 
 
 if __name__ == '__main__':
